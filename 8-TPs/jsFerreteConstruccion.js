d1723bb63b6904190a1ae8d29789cd5744a5cc7c
lucasgabrielsanchez <sanchez_lukas@hotmail.com>
2016-07-06 16:26:10 -0300

    Ejercicio N°2 de (TPs) finalizado.

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,28 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
-
+	//calculamos el perímetro de un rectángulo y lo multiplicamos por 3 para obtener la cantidad de alambre necesaria.
+	var largo=document.getElementById('Largo').value;
+	var ancho=document.getElementById('Ancho').value;
+	var resultado=((largo*2)+(ancho*2))*3;
+	alert("La cantidad necesaria de alambre para hacer 3 hiladas en este terreno es de "+resultado+" metros.");
 }
 function Circulo () 
 {
-	
+	//calculamos el perímetro de un círculo y lo multiplicamos por 3 para obtener la cantidad de alambre necesaria.
+	var radio=document.getElementById('Radio').value;
+	var resultado=2*3.14*radio;
+	alert("La cantidad necesaria de alambre para hacer 3 hiladas en este terreno es de "+resultado+" metros.");
 }
 function Materiales () 
 {
-	
+	//calculamos la superficie del rectángulo para saber cuantos metros cuadrados tiene y por ende, saber cuantas bolsas de
+	//cemento y cal necesitamos.
+	var largo=document.getElementById('Largo').value;
+	var ancho=document.getElementById('Ancho').value;
+	var metrosCuadrados=largo*ancho;
+	//multiplicamos los metros cuadrados por la cantidad de bolsas necesarias por metro. Usamos 2 variables (cal y cemento).
+	var bolsaCemento=metrosCuadrados*2;
+	var bolsaCal=metrosCuadrados*3;
+	alert("Para hacer un contrapiso en este terreno, se necesitan "+bolsaCemento+" bolsas de cemento y "+bolsaCal+" bolsas de cal.");
 }
