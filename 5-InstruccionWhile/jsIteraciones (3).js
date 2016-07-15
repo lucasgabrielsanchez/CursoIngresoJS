28261158abda29cbb3453a24a58951cc2e79c9b8
lucasgabrielsanchez <sanchez_lukas@hotmail.com>
2016-07-15 20:41:23 -0300

    ejercicio 3 realizado para validar contraseñas

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,12 @@
 function Mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
-
+var clave 
+clave = prompt("ingrese el número clave.");
+while(clave!="utn750")
+{
+	alert("Clave incorrecta, vuelva a intentarlo.");
+	clave = prompt("ingrese el número clave.");
+}
 
 }//FIN DE LA FUNCIÓN
