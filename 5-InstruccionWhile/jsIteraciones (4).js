618a1b9ce93578c9d1c18605ee9dd9f69fd156a5
lucasgabrielsanchez <sanchez_lukas@hotmail.com>
2016-07-15 21:54:13 -0300

    ejercicio 4 finalizado

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,15 @@
 function Mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	var numero
+	numero=-1
+	
+	while (numero<0 || numero>=9)
+	{
+		numero = prompt("ingrese un número entre 0 y 10.");
+	}
+
+	document.getElementById('Numero').value = numero;
 
 
 }//FIN DE LA FUNCIÓN
